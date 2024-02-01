c9d6c6b1b10a164abc796f48646773715bc40e6a
Zihlu Wang <zihlu.wang@outlook.com>
2024-02-02 00:51:11 +0800

    Added dependencies and utils.

---
+++ src/main/java/cc/zihluwang/sample/SampleUtil.java
@@ -0,0 +1,20 @@
+package cc.zihluwang.sample;
+
+/**
+ * This is a sample util class which is used to show how to migrate from maven
+ * to gradle in Java library.
+ *
+ * @author Zihlu Wang
+ */
+public class SampleUtil {
+
+    /**
+     * This is a sample method that always returns a {@code true}.
+     *
+     * @return {@code true}
+     */
+    public static boolean sampleMethod() {
+        return true;
+    }
+
+}
